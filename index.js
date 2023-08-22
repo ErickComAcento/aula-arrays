19618224ce546cfe345872fe1f2e722abebe3952
Erick <ericklopesalmeida8@gmail.com>
2023-08-21 21:54:51 -0300

    codigando brabamente

---
+++ index.js
@@ -0,0 +1,34 @@
+/* EXERCÍCIO 1 DE INTERPRETAÇÃO
+let array
+console.log('a. ', array)
+
+array = null
+console.log('b. ', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+
+let i = 0
+console.log('d. ', array[i])
+
+array[i+1] = 19
+console.log('e. ', array)
+
+const valor = array[i+6]
+console.log('f. ', valor)
+
+RESPOSTA = Realmente não consegui entender nada do
+código e precisei executá-lo pra entender (pelo menos
+fui sincero). */
+
+
+
+/* EXERCÍCIO 2 DE INTERPRETAÇÃO
+const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+RESPOSTA = O console vai ser "SUBI NUM ÔNIBUS EM MIRROCOS" "27". */
+
+
+
