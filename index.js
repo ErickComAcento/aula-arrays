52709314d1fba721da7b345d306e2a187edab094
Erick <ericklopesalmeida8@gmail.com>
2023-08-22 21:07:56 -0300

    CODIGUEI DEMAIS HJ

--- index.js
+++ index.js
@@ -32,3 +32,38 @@ RESPOSTA = O console vai ser "SUBI NUM ÔNIBUS EM MIRROCOS" "27". */
 
 
 
+
+
+/* EXERCÍCIO 1 DE ESCRITA DE CÓDIGO
+const emailusuario = prompt("Fala teu email aí.")
+const nomeusuario = prompt("Teu nome agora animal!")
+console.log(`O email ${emailusuario} foi cadastrado com sucesso. Seja bem-vinda(o) ${nomeusuario}!`) */
+
+
+
+/* EXERCÍCIO 2 DE ESCRITA DE CÓDIGO
+const comidas = ["xis", "massa", "pastel", "pizza", "pão"]
+console.log("Essas são minhas comidas preferidas:")
+console.log(comidas)
+comidas[1] = prompt("Tua comida favorita")
+console.log(comidas) */
+
+
+
+/* EXERCÍCIO 3 DE ESCRITA DE CÓDIGO
+const t1 = prompt("Diz uma tarefa aí")
+const t2 = prompt("Diz uma tarefa aí")
+const t3 = prompt("Diz uma tarefa aí")
+const listaDeTarefas = [t1, t2, t3]
+console.log(listaDeTarefas)
+const remover = prompt("Escolhe um número de 0 a 2")
+listaDeTarefas.splice(remover, 1)
+console.log(listaDeTarefas) */
+
+
+
+/* DESAFIO (levemente incompleto kkkkk)
+const frase = prompt("Diz uma frase")
+const separador = frase.split(" ")
+const frases = [separador]
+console.log (frases) */
